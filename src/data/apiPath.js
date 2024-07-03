d9d33abb722d0b1e66fa0775a5ae3f6c74eea830
bhuvan240901 <bhuvan240901@gmail.com>
2024-07-03 15:46:12 +0530

    Changed path to live in api_url

--- src/data/apiPath.js
+++ src/data/apiPath.js
@@ -1,3 +1,2 @@
-export const API_URL="http://localhost:5000"
-
-// export const API_URL="https://backend-nodejs-quickbite.onrender.com"
+// export const API_URL="http://localhost:5000"
+export const API_URL="https://backend-nodejs-quickbite.onrender.com"
